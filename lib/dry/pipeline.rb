1999b29d2caed1d7ee443e13c35b7db4d4146314
Piotr Solnica <piotr.solnica@gmail.com>
2015-08-18 17:22:20 +0200

    Add Pipeline mixing which adds `>>` operator

--- lib/dry/pipeline.rb
+++ lib/dry/pipeline.rb
@@ -2,6 +2,39 @@ require "dry/pipeline/version"
 
 module Dry
   module Pipeline
-    # Your code goes here...
+    class Composite
+      attr_reader :left
+
+      attr_reader :right
+
+      # @api private
+      def initialize(left, right)
+        @left = left
+        @right = right
+      end
+
+      def call(value)
+        right.(left.(value))
+      end
+      alias_method :[], :call
+
+      def >>(other)
+        self.class.new(self, other)
+      end
+    end
+
+    attr_reader :fn
+
+    def initialize(fn, *)
+      @fn = fn
+    end
+
+    def call(input)
+      fn.(input)
+    end
+
+    def >>(other)
+      Composite.new(self, other)
+    end
   end
 end
--- spec/dry/pipeline_spec.rb
+++ spec/dry/pipeline_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
 describe Dry::Pipeline do
-  it 'has a version number' do
-    expect(Dry::Pipeline::VERSION).not_to be nil
+  let(:fn1) do
+    Class.new { include Dry::Pipeline }.new(-> str { str.reverse })
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  let(:fn2) do
+    Class.new { include Dry::Pipeline }.new(-> str { str.upcase })
+  end
+
+  let(:fn3) do
+    Class.new { include Dry::Pipeline }.new(-> str { str.split(' ') })
+  end
+
+  let(:op) { -> str { str.upcase } }
+
+  it 'provides >> operator' do
+    pipeline = fn1 >> fn2
+
+    expect(pipeline['hello world']).to eql('DLROW OLLEH')
+
+    pipeline >>= fn3
+
+    expect(pipeline['hello world']).to eql(%w(DLROW OLLEH))
   end
 end
